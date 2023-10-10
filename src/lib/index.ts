9d7ef20497f2fa20effb66b24c28db4b85f5368c
Typogram <cz10979@umbc.edu>
2023-10-10 00:39:58 -0400

    add simple CodePreview

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ import Logo from './Logo.svelte';
 import Icon from './Icon.svelte';
 import Sidebar from './Sidebar.svelte';
 import Header from './Header.svelte';
+import CodePreview from './CodePreview.svelte';
 
-export { Logo, Icon, ThemeSwitch, Sidebar, Header };
+export { Logo, Icon, ThemeSwitch, Sidebar, Header, CodePreview };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+import { CodePreview } from '$lib';
 </script>
 
 <div class="h-full bg-surface-50-900-token p-4">
   <h1 class="h1">Welcome to Coding Font</h1>
+  <CodePreview />
 </div>
