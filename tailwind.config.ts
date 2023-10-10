e9f743357b703c1ccd4d03478c6f7114a62f5652
Typogram <cz10979@umbc.edu>
2023-10-09 21:56:06 -0400

    Setup sveltekit, tailwind, skeleton

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+const config = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    join(
+      require.resolve('@skeletonlabs/skeleton'),
+      '../**/*.{html,js,svelte,ts}'
+    )
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    skeleton({
+      themes: {
+        preset: [
+          { name: 'modern', enhancements: true },
+          { name: 'seafoam', enhancements: true },
+          { name: 'wintry', enhancements: true },
+          { name: 'skeleton', enhancements: true },
+          { name: 'crimson', enhancements: true },
+          { name: 'rocket', enhancements: true },
+          { name: 'vintage', enhancements: true },
+          { name: 'sahara', enhancements: true },
+          { name: 'hamlindigo', enhancements: true },
+          { name: 'gold-nouveau', enhancements: true }
+        ]
+      }
+    })
+  ]
+} satisfies Config;
+
+export default config;
