b0a588b0f66f7e7b8df0dee7d00ab03040faf2c9
Typogram <cz10979@umbc.edu>
2023-10-18 14:58:52 -0400

    add global navigation

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
+import { page } from '$app/stores';
+import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
 import { IconMenu, Logo, ThemeSwitch } from '$lib';
 import { menuOpen } from '$lib/store';
 </script>
 
 <div
-  class="flex flex-row items-center p-4 gap-4 bg-surface-100-800-token border-b border-surface-400 dark:border-surface-500">
-  <button
-    class="btn-icon variant-soft flex lg:hidden"
-    on:click="{() => {
-      $menuOpen = !$menuOpen;
-    }}">
-    <IconMenu size="24" />
-  </button>
-  <a href="/"><Logo class="w-auto h-[3rem]" /></a>
-  <div class="flex-grow"></div>
+  class="flex flex-row items-center justify-between p-4 gap-4 bg-surface-100-800-token border-b border-surface-400 dark:border-surface-700">
+  <div class="flex flex-row gap-4">
+    <button
+      class="btn-icon variant-soft flex lg:hidden"
+      on:click="{() => {
+        $menuOpen = !$menuOpen;
+      }}">
+      <IconMenu size="24" />
+    </button>
+    <a href="/"><Logo class="w-auto h-[3rem]" /></a>
+  </div>
+
+  <TabGroup
+    border="border-none h3 relative"
+    active="after:absolute after:-translate-x-1/2 after:bottom-0 after:w-4 after:h-1 after:bg-[currentColor] after:rounded-full">
+    <TabAnchor href="/game" selected="{$page.url.pathname === '/game'}"
+      >Game</TabAnchor>
+    <TabAnchor href="/" selected="{$page.url.pathname === '/'}"
+      >Browse</TabAnchor>
+  </TabGroup>
+
   <ThemeSwitch />
 </div>
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+import { AppShell } from '@skeletonlabs/skeleton';
+import { Header, Sidebar, FontHeader, Controls, MonacoEditor } from '$lib';
+import {
+  selectedTheme,
+  fontSize,
+  fontFamily,
+  fontFamilyRight,
+  fontLigatures,
+  searchTerm
+} from '$lib/store';
+
+export let data;
+let { fonts } = data;
+
+function getFontByFamilyName(familyName: string) {
+  return fonts.find((font) => font.family === familyName);
+}
+</script>
+
+<AppShell
+  slotSidebarLeft="flex relative resize-x min-w-0 w-0 lg:w-[30rem] lg:min-w-[16rem] !overflow-visible"
+  slotHeader="z-30">
+  <svelte:fragment slot="header">
+    <Header />
+  </svelte:fragment>
+  <svelte:fragment slot="sidebarLeft">
+    <Sidebar />
+  </svelte:fragment>
+  <svelte:fragment slot="pageHeader">
+    <Controls />
+  </svelte:fragment>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 h-full bg-surface-50-900-token p-4 gap-4">
+    <div class="flex flex-col gap-4" class:col-span-2="{!$fontFamilyRight}">
+      <FontHeader font="{getFontByFamilyName($fontFamily)}" />
+      <MonacoEditor
+        class="rounded-container-token overflow-hidden"
+        fontSize="{$fontSize}"
+        fontFamily="{$fontFamily}"
+        fontLigatures="{$fontLigatures}"
+        themeName="{$selectedTheme}" />
+    </div>
+    {#if $fontFamilyRight}
+      <div class="hidden md:flex flex-col gap-4 relative">
+        <FontHeader font="{getFontByFamilyName($fontFamilyRight)}" />
+        <MonacoEditor
+          class="rounded-container-token overflow-hidden"
+          fontSize="{$fontSize}"
+          fontFamily="{$fontFamilyRight}"
+          fontLigatures="{$fontLigatures}"
+          themeName="{$selectedTheme}" />
+        <button
+          class="btn variant-filled-surface absolute bottom-4 self-center"
+          on:click="{() => {
+            $fontFamilyRight = '';
+          }}">Clear Comparison</button>
+      </div>
+    {/if}
+  </div>
+</AppShell>
