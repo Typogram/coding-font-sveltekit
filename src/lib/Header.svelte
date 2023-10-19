295c79963a29fb4daa7e7c6e20d406d98f14dde4
Typogram <cz10979@umbc.edu>
2023-10-18 23:42:24 -0400

    switch page between game and browse

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -7,9 +7,9 @@ import { menuOpen } from '$lib/store';
 
 <div
   class="flex flex-row items-center justify-between p-4 gap-4 bg-surface-100-800-token border-b border-surface-400 dark:border-surface-700">
-  <div class="flex flex-row gap-4">
+  <div class="flex gap-4 items-center justify-center">
     <button
-      class="btn-icon variant-soft flex lg:hidden"
+      class="btn-icon variant-soft lg:hidden"
       on:click="{() => {
         $menuOpen = !$menuOpen;
       }}">
@@ -19,14 +19,13 @@ import { menuOpen } from '$lib/store';
   </div>
 
   <TabGroup
-    flex="flex"
+    flex="hidden md:flex"
     padding="px-2"
     border="border-none h3 relative"
     hover=""
     active="before:content-['<'] after:content-['>']">
-    <TabAnchor href="/game" selected="{$page.url.pathname === '/game'}"
-      >Game</TabAnchor>
-    <TabAnchor href="/" selected="{$page.url.pathname === '/'}"
+    <TabAnchor href="/" selected="{$page.url.pathname === '/'}">Game</TabAnchor>
+    <TabAnchor href="/browse" selected="{$page.url.pathname === '/browse'}"
       >Browse</TabAnchor>
   </TabGroup>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,57 @@
 <script lang="ts">
+import { onMount, tick } from 'svelte';
 import { AppShell } from '@skeletonlabs/skeleton';
 import {
   Header,
-  SearchBar,
-  FontTable,
   Sidebar,
   FontHeader,
   Controls,
-  MonacoEditor
+  MonacoEditor,
+  PlayerBadge,
+  WinnerBadge,
+  createGame,
+  createConfetti
 } from '$lib';
-import {
-  selectedTheme,
-  fontSize,
-  fontFamily,
-  fontFamilyRight,
-  fontLigatures,
-  searchTerm
-} from '$lib/store';
+import { showName, selectedTheme, fontSize, fontLigatures } from '$lib/store';
 
 export let data;
 let { fonts } = data;
+let game;
+let currentBracket;
+
+onMount(() => {
+  startGame();
+});
+
+function startGame() {
+  game = createGame(fonts);
+  currentBracket = game.startGame();
+}
 
 function getFontByFamilyName(familyName: string) {
   return fonts.find((font) => font.family === familyName);
 }
 
-$: if ($searchTerm) {
-  fonts = data.fonts.filter((font) =>
-    font.family.toLowerCase().includes($searchTerm.toLowerCase())
-  );
-} else {
-  fonts = data.fonts;
+async function chooseWinner(player) {
+  currentBracket = game.setWinner(player);
+  game = game;
+  if (currentBracket?.winner) {
+    createConfetti();
+  }
+  await tick();
+  scrollToBracket();
+}
+
+function scrollToBracket() {
+  const winnerElement = document.querySelector('.winner-candidate');
+  if (winnerElement) {
+    winnerElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+
+  const activeBracket = document.querySelector('.font-bracket.active');
+  if (activeBracket) {
+    activeBracket.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
 }
 </script>
 
@@ -42,38 +63,116 @@ $: if ($searchTerm) {
   </svelte:fragment>
   <svelte:fragment slot="sidebarLeft">
     <Sidebar>
-      <SearchBar />
-      <FontTable fonts="{fonts}" />
+      <button class="btn variant-filled-primary" on:click="{startGame}"
+        >Restart Game</button>
+      {#if game?.rounds.length}
+        <div class="table-container rounded-none p-2">
+          <div class="font-brackets">
+            {#each game.rounds as round, index (round)}
+              {#if game.finalRound === index}
+                <div class="round-winner">
+                  <WinnerBadge>{round[0].winner.family}</WinnerBadge>
+                </div>
+              {:else}
+                <div class="{`round-${index + 1}`}">
+                  {#each round as bracket (bracket)}
+                    <div
+                      class="font-bracket"
+                      class:active="{bracket === currentBracket}">
+                      {#each bracket?.players as font (font)}
+                        <PlayerBadge
+                          class="{bracket?.winner?.family == font.family
+                            ? 'variant-ghost-primary'
+                            : 'variant-ghost-surface'}">
+                          <span style="font-family: '{font.family}'">
+                            {$showName ? font.family : 'ABC abc 123'}
+                          </span>
+                        </PlayerBadge>
+                      {/each}
+                      <div
+                        class="line-bracket {bracket.players.length === 1
+                          ? 'bottom-1/2'
+                          : ''}">
+                      </div>
+                    </div>
+                  {/each}
+                </div>
+              {/if}
+            {/each}
+          </div>
+        </div>
+      {/if}
     </Sidebar>
   </svelte:fragment>
   <svelte:fragment slot="pageHeader">
     <Controls />
   </svelte:fragment>
   <div
-    class="grid grid-cols-1 md:grid-cols-2 h-full bg-surface-50-900-token p-4 gap-4">
-    <div class="flex flex-col gap-4" class:col-span-2="{!$fontFamilyRight}">
-      <FontHeader font="{getFontByFamilyName($fontFamily)}" />
-      <MonacoEditor
-        class="rounded-container-token overflow-hidden"
-        fontSize="{$fontSize}"
-        fontFamily="{$fontFamily}"
-        fontLigatures="{$fontLigatures}"
-        themeName="{$selectedTheme}" />
-    </div>
-    {#if $fontFamilyRight}
-      <div class="hidden md:flex flex-col gap-4 relative">
-        <FontHeader font="{getFontByFamilyName($fontFamilyRight)}" />
+    class="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 h-full bg-surface-50-900-token p-4 gap-4">
+    {#if currentBracket?.players?.length}
+      <div class="flex flex-col gap-4 relative">
+        <FontHeader
+          font="{getFontByFamilyName(currentBracket.players[0].family)}" />
         <MonacoEditor
           class="rounded-container-token overflow-hidden"
           fontSize="{$fontSize}"
-          fontFamily="{$fontFamilyRight}"
+          fontFamily="{currentBracket.players[0].family}"
           fontLigatures="{$fontLigatures}"
           themeName="{$selectedTheme}" />
         <button
-          class="btn variant-filled-surface absolute bottom-4 self-center"
-          on:click="{() => {
-            $fontFamilyRight = '';
-          }}">Clear Comparison</button>
+          class="btn variant-filled-primary absolute bottom-10 self-center shadow-xl"
+          on:click="{chooseWinner(currentBracket.players[0])}">Choose</button>
+      </div>
+      <div class="flex flex-col gap-4 relative">
+        <FontHeader
+          font="{getFontByFamilyName(currentBracket.players[1].family)}" />
+        <MonacoEditor
+          class="rounded-container-token overflow-hidden"
+          fontSize="{$fontSize}"
+          fontFamily="{currentBracket.players[1].family}"
+          fontLigatures="{$fontLigatures}"
+          themeName="{$selectedTheme}" />
+        <button
+          class="btn variant-filled-primary absolute bottom-10 self-center shadow-xl"
+          on:click="{chooseWinner(currentBracket.players[1])}">Choose</button>
+      </div>
+    {:else if currentBracket?.winner}
+      <div
+        class="relative p-6 md:p-10 col-span-1 row-span-2 md:col-span-2 md:row-span-1 text-center border-4 border-surface-900-50-token bg-surface-50-900-token">
+        <img
+          class="absolute bottom-0 mx-auto left-0 right-0 opacity-60"
+          src="/trophy.png"
+          alt="Trophy of Font"
+          width="400" />
+        <div
+          class="p-4 md:p-10 relative max-w-5xl mx-auto flex flex-col gap-12">
+          <div
+            class="border-t-2 pt-10 border-surface-700-200-token flex flex-col gap-6 tracking-widest">
+            <h2 class="h2">CERTIFICATE OF ABSOLUTE AWESOMENESS</h2>
+            <h4 class="h4">HEREBY UNLEASHED UPON</h4>
+          </div>
+          <div
+            class="text-4xl md:text-6xl my-4"
+            style="font-family: '{currentBracket?.winner.family}'">
+            {currentBracket?.winner.family}
+          </div>
+          <h4 class="h4">
+            For mastering the art of bézier curve pageantry, where zeros, arrows
+            and curly brackets stand heroic in a 10-hour coding crusade! For
+            turning each keystroke into a clear, courageous character,
+            triumphing in the tumultuous tournament of type!
+          </h4>
+          <div class="justify-between mt-20 hidden md:flex">
+            <div>
+              <p class="mb-2">__________________________</p>
+              <p class="text-center">HEAD OF DEPARTMENT</p>
+            </div>
+            <div>
+              <p class="mb-2">__________________________</p>
+              <p class="text-center">COORDINATOR</p>
+            </div>
+          </div>
+        </div>
       </div>
     {/if}
   </div>
